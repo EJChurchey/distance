b9825ebe111318d9556d5e9d195c3e7fd0c10f46
Your Name <everettchurchey@crbc.edu>
2015-04-29 09:51:04 -0400

    distance

--- distance/main.m
+++ distance/main.m
@@ -8,20 +8,23 @@
 
 #import <Foundation/Foundation.h>
 #import <math.h>
-    double distance (double x1, double y1, double x2, double y2){
-        double x3 = x2 - x1;
-        double y3 = y2 - y1;
-        double x = pow (x3,2);
-        double y = pow (y3,2);
-        double xy = x = y;
-        return sqrt (xy);
+double distance (double x1, double y1, double x2, double y2){
+    double x3 = x2 - x1;
+    double y3 = y2 - y1;
+    double x = pow (x3,2);
+    double y = pow (y3,2);
+    double xy = x = y;
+    return sqrt (xy);
     }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        const float ERROR_THRESHOLD=0.05;
+        if (distance(0, 0, 0, 1)-1.0 > ERROR_THRESHOLD){
+            NSLog (@"Invalid Answer");
+            
+        }
         
-        // insert code here...
-        NSLog(@"HUH Really");
     }
     return 0;
 }
